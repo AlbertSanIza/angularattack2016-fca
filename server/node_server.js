789d9c11c214e2e3b43fbfc72427178bb4960e5e
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-14 15:55:24 -0700

    yeey it work!

--- server/node_server.js
+++ server/node_server.js
@@ -1,11 +1,20 @@
-var app = require('express').createServer();
+var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
+var fs = require('fs');
 
-app.listen(80);
+app.listen(3000);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
