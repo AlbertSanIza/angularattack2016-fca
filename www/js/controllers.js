5aeb2b4d589204009ff0dda02d01ba0d809a0d65
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-13 22:31:58 -0700

    Settings modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,18 @@
 angular.module('starter.controllers', [])
 
 .controller('MainCtrl', function($scope, $ionicModal) {
-  
+
+  $ionicModal.fromTemplateUrl('templates/settings.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.settingsModal = modal;
+  });
+  $scope.openSettingsModal = function() {
+    $scope.settingsModal.show();
+  };
+  $scope.closeSettingsModal = function() {
+    $scope.settingsModal.hide();
+  };
+
 })
