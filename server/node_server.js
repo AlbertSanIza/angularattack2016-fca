745cf695b68650f562619a9d596041d1e334adf5
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-14 15:58:55 -0700

    updated to chat tutorial

--- server/node_server.js
+++ server/node_server.js
@@ -1,20 +1,11 @@
-var app = require('http').createServer(handler)
+var app = require('express').createServer();
 var io = require('socket.io')(app);
-var fs = require('fs');
 
-app.listen(3000);
+app.listen(80);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
