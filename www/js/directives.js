ed75bdab3c1a1ffec146ffbf28f7e3222688a143
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-13 22:01:04 -0700

    directive is up and running

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,18 @@
 angular.module('starter.directives', [])
 
-.directive('solarSystem', ['', function() {
-  
-}])
+.directive('solarSystem', function() {
+  return {
+    'restrict': 'E',
+    'template': '<h1>solarSystem</h1>',
+    'scope': {
+      'stereoEffect': '=',
+      'landscapeMode': '=',
+      'moveStarFighter': '=',
+      'planetsSpeed': '='
+    },
+    'link': link
+  };
+  function link($scope, $element, $attr) {
+    
+  };
+})
