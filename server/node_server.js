0376c7e549ac8b23a3c36d46e3450f8d6c175845
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-14 16:05:49 -0700

    now we can chat!

--- server/node_server.js
+++ server/node_server.js
@@ -1,15 +1,14 @@
-var app = require('express').createServer();
-var io = require('socket.io')(app);
-
-app.listen(80);
-
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
+});
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
