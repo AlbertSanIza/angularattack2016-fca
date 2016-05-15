4ee35ebc1448315aee146c7c70551de512791b30
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-15 11:02:30 -0700

    updated server to sync all views

--- server/node_server.js
+++ server/node_server.js
@@ -1,39 +1,47 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-var integer = 0;
-var time = 1/10000*1.5;
 
-function timezone()
-{
-	if(integer>=2147483646){
-		integer=0;
-	}
-   integer=integer+.0001;
-   //console.log(integer);
-   setTimeout(timezone, time);
-}
-timezone();
+var theTime = 0;
+var theTimeFraction = 25;
 
+function theTimeChange() {
+  if(theTime >= 2147483600) {
+    theTime = 0;
+  }
+  theTime = theTime + .0009;
+  setTimeout(theTimeChange, theTimeFraction);
+};
+theTimeChange();
 
-function timesync()
-{
-   io.emit('time', integer);
-   setTimeout(timesync, 5000);
-}
-timesync();
+function timeCheck() {
+  io.emit('time check', theTime);
+  setTimeout(timeCheck, 500);
+};
+timeCheck();
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+function theTimeSync() {
+  io.emit('theTime', theTime);
+  setTimeout(theTimeSync, 5000);
+};
+theTimeSync();
+
+io.on('connection', function(socket) {
+
+  io.emit('theTime', theTime);
+
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
-	io.emit('chat message', integer);
-	io.emit('time', integer);
   });
-});
+  socket.on('time check', function(msg) {
+    io.emit('time check', t);
+  });
 
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
