a949acedf02c948862008f98b0f084524c1c24a0
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-15 02:51:42 -0700

    added location and functionality

--- app/app.js
+++ app/app.js
@@ -1,10 +1,19 @@
 angular.module('starter', ['ionic'])
 
 .controller('MainCtrl', function($scope, $ionicSlideBoxDelegate) {
+  $scope.slideIndex = 0;
   $scope.slidePrevious = function() {
     $ionicSlideBoxDelegate.previous(300);
   };
   $scope.slideNext = function() {
     $ionicSlideBoxDelegate.next(300);
+  };
+
+  $scope.slideChanged = function(index) {
+    $scope.slideIndex = index;
+  };
+
+  $scope.startApp = function() {
+    window.location.href = "http://fca.2016.angularattack.io/www/"
   };
 })
