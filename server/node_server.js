1d86e27e039c155291f9a72c5b2dc67269b94855
arbreton <andrebreton@live.com>
2016-05-14 17:27:40 -0700

    timer

--- server/node_server.js
+++ server/node_server.js
@@ -4,6 +4,19 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+var integer = 0;
+var time = 1/10*2;
+
+function timezone()
+{
+   integer=integer+.0001;
+   console.log(integer);
+   setTimeout(timezone, time);
+}
+
+timezone();
+
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
